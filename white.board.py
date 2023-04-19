52c87d32cf72a662ecf5a6d7bdb7326a120de649
Andy Santana <andysantana@Andys-MacBook-Pro.local>
2023-04-18 20:31:20 -0500

    week3 day2 hw

---
+++ white.board.py
@@ -0,0 +1,25 @@
+# attackers = [2,4,5,6,]
+# defenders = [3,4,4,3]
+# survivors_a= []
+# survivors_b = []
+# for a_power in range(len(attackers)):
+#     if attackers[a_power] > defenders[x]:
+#         survivors_a.append(attackers[a_power])
+#         if attackers[a_power] == 0:
+#             survivors_a == 0
+#         elif defenders[x] == 0:
+#             survivors_b == 0 
+#     elif attackers[a_power] == defenders[x]:
+#         survivors_a.append(0)
+#         survivors_b.append(0)
+#     else:
+#         defenders[X]
+#         survivors_b.append(attackers[a_power])        
+#     if attackers
+# #loop through each soldier and compare the lists (high value wins)
+# #if tie (both die)
+# #if value == 0 (soldier dies)
+# #To win the defender need to have more survivors 
+# #if same survivors
+#     #winner is the team with the highest attack power
+# if both sizes have the same attack power return true
